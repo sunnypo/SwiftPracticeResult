ec18724bdc441ad3a39c866f7e8021f5310a014d
sunnypo <46382526+sunnypo@users.noreply.github.com>
2020-01-18 11:26:02 -0500

    Update 区间main.swift

--- sunnypo/005税务计算器/main.swift
+++ sunnypo/005税务计算器/main.swift
@@ -1,8 +1,8 @@
 let number = askForNumber("Please enter your annual salary(in dollars)")
 var resultQC = 0
 var resultUN = 0
-let percentUN = [0,3300,2900,2500,1500]
-let valueUN = [0,210371,95259,47630,0]
+let percentUN = [0,3300,2900,2600,2050,1500]
+let valueUN = [0,210371,147667,95259,47630,0]
 let percentQC = [0,2575,2400,2000,1500]
 let valueQC = [0,106555,87575,43790,0]
 var a = 0
@@ -20,7 +20,7 @@ for i in 1 ... 4 {
     }
 }
 a = 0
-for i in 1 ... 4 {
+for i in 1 ... 5 {
     resultUN += caculate(value: valueUN[i], percent: percentUN[i], i: valueUN[i-1])
     if resultUN > 0{
         a = 1
